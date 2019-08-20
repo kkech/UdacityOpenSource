ea3408a01e1d38d23fa2a5ad0f691c1d47479d23
Seerat Jaura <46867415+JauraSeerat@users.noreply.github.com>
2019-08-20 00:36:52 -0500

    Wonder_Vision_Face_Detection Project (#69)
    This pull request contains the project carried out by the Wonder_Vision_Face_Detection sub-group. I and my team members participated in the Udacity AI challenge, more information can be found in the Readme file attached in our folder.

---
+++ Wonder_Vision_Face_Detection/Tutorial_Implementations/Agata(@Agata [OR, USA])/train_model.py
@@ -0,0 +1,44 @@
+# USAGE
+# python train_model.py --embeddings output/embeddings.pickle \
+#	--recognizer output/recognizer.pickle --le output/le.pickle
+
+# import the necessary packages
+from sklearn.preprocessing import LabelEncoder
+from sklearn.svm import SVC
+import argparse
+import pickle
+
+# construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-e", "--embeddings", required=True,
+	help="path to serialized db of facial embeddings")
+ap.add_argument("-r", "--recognizer", required=True,
+	help="path to output model trained to recognize faces")
+ap.add_argument("-l", "--le", required=True,
+	help="path to output label encoder")
+args = vars(ap.parse_args())
+
+# load the face embeddings
+print("[INFO] loading face embeddings...")
+data = pickle.loads(open(args["embeddings"], "rb").read())
+
+# encode the labels
+print("[INFO] encoding labels...")
+le = LabelEncoder()
+labels = le.fit_transform(data["names"])
+
+# train the model used to accept the 128-d embeddings of the face and
+# then produce the actual face recognition
+print("[INFO] training model...")
+recognizer = SVC(C=1.0, kernel="linear", probability=True)
+recognizer.fit(data["embeddings"], labels)
+
+# write the actual face recognition model to disk
+f = open(args["recognizer"], "wb")
+f.write(pickle.dumps(recognizer))
+f.close()
+
+# write the label encoder to disk
+f = open(args["le"], "wb")
+f.write(pickle.dumps(le))
+f.close()
